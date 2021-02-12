f19fa62e2537ce0bf611937982c8196f2fd51cae
dluo01 <77885122+dluo01@users.noreply.github.com>
2021-02-12 20:56:09 +0800

    Tamagotchi

---
+++ tamagotchi/random.js
@@ -0,0 +1,60 @@
+//https://www.geeksforgeeks.org/how-to-append-html-code-to-a-div-using-javascript/ 
+//http://www.javascriptkit.com/javatutors/randomorder.shtml
+
+
+//HOW TO:
+//copy and paste the html code you have for each image into the images=[] below. Make sure there are no line breaks. I've included some examples. 	
+
+var images=new Array()
+images[0]='<div class="grid-item"><a href="bio structure.html"><img class="tama" border="0" src="images/tb/01.png"></a><a href="population explosion.html" target="_blank"><p>$59.99</p></a></div>'
+images[1]='<div class="grid-item"><a href="instructions.html"><img class="tama" border="0" src="images/tb/02.png"></a><a href="population explosion.html" target="_blank"><p>$59.99</p></a></div>'
+images[2]='<div class="grid-item"><a href="marriage.html"><img class="tama" border="0" src="images/tb/03.png"></a><a href="population explosion.html" target="_blank"><p>$59.99</p></a></div>'
+images[3]='<div class="grid-item"><a href="extinction1.html"><img class="tama" border="0" src="images/tb/04.png"></a><a href="population explosion.html" target="_blank"><p>$19.99</p></a></div>'
+images[4]='<div class="grid-item"><a href="extinction2.html"><img class="tama" border="0" src="images/tb/05.png"></a><a href="population explosion.html" target="_blank"><p>$24.99</p></a></div>'
+images[5]='<div class="grid-item"><a href="extinction3.html"><img class="tama" border="0" src="images/tb/06.png"></a><a href="population explosion.html" target="_blank"><p>$59.99</p></a></div>'
+images[6]='<div class="grid-item"><a href="extinction4.html"><img class="tama" border="0" src="images/tb/07.png"></a><a href="population explosion.html" target="_blank"><p>$30.99</p></a></div>'
+images[7]='<div class="grid-item"><a href="https://www.nature.com/articles/d41586-019-01448-4" target="_blank"><img class="tama" border="0" src="images/tb/41.png"></a><a href="population explosion.html" target="_blank"><p>$19.99</p></a></div>'
+images[8]='<div class="grid-item"><a href="https://naturalhistory.si.edu/education/teaching-resources/paleontology/extinction-over-time" target="_blank"><img class="tama" border="0" src="images/tb/09.png"></a><a href="population explosion.html" target="_blank"><p>$19.99</p></a></div>'
+images[9]='<div class="grid-item"><a href="https://shesdevilish.tripod.com/grave.html" target="_blank"><img class="tama" border="0" src="images/tb/10.png"></a><a href="population explosion.html" target="_blank"><p>$19.99</p></a></div>'
+images[10]='<div class="grid-item"><a href="tamagotchi.html"><img class="tama" border="0" src="images/tb/11.png"></a><a href="population explosion.html" target="_blank"><p>$59.99</p></a></div>'
+images[11]='<div class="grid-item"><a href="https://www.digitaltrends.com/cool-tech/how-tamagotchi-shaped-tech/" target="_blank"><img class="tama" border="0" src="images/tb/12.png"></a><a href="population explosion.html" target="_blank"><p>$59.99</p></a></div>'
+images[12]='<div class="grid-item"><a href="https://reuters.screenocean.com/record/_Q6i2YYeHLXxgt0iBiOi1iWp" target="_blank"><img class="tama" border="0" src="images/tb/13.png"></a><a href="population explosion.html" target="_blank"><p>$19.99</p></a></div>'
+images[13]='<div class="grid-item"><a href="tamagotchi.html"><img class="tama" border="0" src="images/tb/14.png"></a><a href="population explosion.html" target="_blank"><p>$59.99</p></a></div>'
+images[14]='<div class="grid-item"><a href="Petz.html"><img class="tama" border="0" src="images/tb/45.png"></a><a href="population explosion.html" target="_blank"><p>$19.99</p></a></div>'
+images[15]='<div class="grid-item"><a href="Nintendogs.html"><img class="tama" border="0" src="images/tb/16.png"></a><a href="population explosion.html" target="_blank"><p>$59.99</p></a></div>'
+images[16]='<div class="grid-item"><a href="save Pet Society.html"><img class="tama" border="0" src="images/tb/17.png"></a><a href="population explosion.html" target="_blank"><p>$30.99</p></a></div>'
+images[17]='<div class="grid-item"><a href="Tamagotchi Commercial.html"><img class="tama" border="0" src="images/tb/18.png"></a><a href="population explosion.html" target="_blank"><p>$59.99</p></a></div>'
+images[18]='<div class="grid-item"><a href="generations.html"><img class="tama" border="0" src="images/tb/19.png"></a><a href="population explosion.html" target="_blank"><p>$24.99</p></a></div>'
+images[19]='<div class="grid-item"><a href="Robotica.html"><img class="tama" border="0" src="images/tb/20.png"></a><a href="population explosion.html" target="_blank"><p>$24.99</p></a></div>'
+images[20]='<div class="grid-item"><a href="Funeral for Tamagotchi.html"><img class="tama" border="0" src="images/tb/21.png"></a><a href="population explosion.html" target="_blank"><p>$24.99</p></a></div>'
+images[21]='<div class="grid-item"><a href="garden.html"><img class="tama" border="0" src="images/tb/22.png"></a><a href="population explosion.html" target="_blank"><p>$24.99</p></a></div>'
+images[22]='<div class="grid-item"><a href="e-girls.html"><img class="tama" border="0" src="images/tb/29.png"></a><a href="population explosion.html" target="_blank"><p>$24.99</p></a></div>'
+images[23]='<div class="grid-item"><a href="species guide.html"><img class="tama" border="0" src="images/tb/24.png"></a><a href="population explosion.html" target="_blank"><p>$24.99</p></a></div>'
+images[24]='<div class="grid-item"><a href="Parenting Manual.html"><img class="tama" border="0" src="images/tb/25.png"></a><a href="population explosion.html" target="_blank"><p>$24.99</p></a></div>'
+images[25]='<div class="grid-item"><a href="Parenting Manual.html"><img class="tama" border="0" src="images/tb/32.png"></a><a href="population explosion.html" target="_blank"><p>$24.99</p></a></div>'
+images[26]='<div class="grid-item"><a href="Love it, Feed it, Mourn it.html"><img class="tama" border="0" src="images/tb/44.png"></a><a href="population explosion.html" target="_blank"><p>$19.99</p></a></div>'
+images[27]='<div class="grid-item"><a href="https://onezero.medium.com/the-tamagotchi-hacking-communitys-quest-to-cheat-death-dc3e7ee27a0f" target="_blank"><img class="tama" border="0" src="images/tb/38.png"></a><a href="population explosion.html" target="_blank"><p>$19.99</p></a></div>'
+images[28]='<div class="grid-item"><a href="Play God, Catch Hell.html"><img class="tama" border="0" src="images/tb/34.png"></a><a href="population explosion.html" target="_blank"><p>$19.99</p></a></div>'
+images[29]='<div class="grid-item"><a href="Family Tree.html"><img class="tama" border="0" src="images/tb/46.png"></a><a href="population explosion.html" target="_blank"><p>$19.99</p></a></div>'
+
+
+var i=0
+//variable used to contain controlled random number 
+var random
+//while all of array elements haven't been cycled thru
+
+window.onload = function(){
+//wait until page loads
+while (i<images.length){
+    //generate random num between 0 and arraylength-1
+    random=Math.floor(Math.random()*images.length)
+    //if element hasn't been marked as "selected"
+    if (images[random]!="selected"){
+    	let aaa = document.getElementById("grid-container");
+        aaa.innerHTML += (images[random]);
+        //mark element as selected
+        images[random]="selected"
+        i++
+    }
+}
+}
